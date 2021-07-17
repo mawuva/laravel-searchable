5cfeebefb7f4c3b5e29adb49a76514a9a97ddbe4
Seddor Ephraim <seddorephraim7@gmail.com>
2021-07-17 03:50:01 +0100

    Add scopeSearch method in Searchable Trait to allow search on models

--- src/Searchable.php
+++ src/Searchable.php
@@ -2,7 +2,21 @@
 
 namespace Mawuekom\LaravelSearchable;
 
-class LaravelSearchable
+use Illuminate\Database\Eloquent\Builder;
+
+trait Searchable
 {
-    // Build your next great package.
+    /**
+     * Scope a query to make search on model.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param array $attributes
+     * @param string $searchTerm
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function search($query, array $attributes, string $searchTerm): Builder
+    {
+        return $query ->whereLike($attributes, $searchTerm);
+    }
 }
